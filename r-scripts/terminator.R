9afc7d6609bd40e429ba57a44bdb2f7f5f4f2c14
charcoalcharts <39935741+charcoalcharts@users.noreply.github.com>
2023-02-15 15:36:53 -0500

    Update terminator.R

--- r-scripts/terminator.R
+++ r-scripts/terminator.R
@@ -96,7 +96,8 @@ terminator <- function(time, from = -180-360, to = 180+360, by = 1) {
   }) %>%
     plyr::rbind.fill()
 
-bo=as.numeric(format(as.Date(as.POSIXct(time,origin="1970-1-1",tz="GMT"))-22,"%m"))
+bo=as.POSIXct(as.numeric(time),origin="1970-1-1"); attr(b,"tzone")="UTC"
+bo=as.numeric(format(as.Date(bo)-22,"%m"))
 df$night=ifelse(bo%in%3:8,-1,1)
 df
 }
