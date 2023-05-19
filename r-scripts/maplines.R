30b88360ae2a11740d3b7eac1a7b82241bc20311
charcoalcharts <39935741+charcoalcharts@users.noreply.github.com>
2023-05-19 13:32:57 -0400

    Update maplines.R

--- r-scripts/maplines.R
+++ r-scripts/maplines.R
@@ -9,7 +9,7 @@ blines=function(x,y,rcol="black",fcol="blue",rlwd=1,flwd=2,drive=T,data=F,...){
   loc=paste(paste(x[1],y[1],sep=","),paste(x[2],y[2],sep=","),sep=";")
   url=paste0("http://router.project-osrm.org/route/v1/driving/",loc,"?geometries=geojson&overview=full")
   r0=GET(url); r0=content(r0); if(r0$code=="Ok" & drive){rx=unlist(r0$routes[[1]]$geometry$coordinates); n=length(rx)/2; sn=seq(1,n,by=10)
-  long=rx[1:n*2-1][sn]+runif(1,-1,1)/10; lat=rx[1:n*2][sn]+runif(1,-1,1)/10
+  long=rx[1:n*2-1][sn]; lat=rx[1:n*2][sn]
   if(data) data.frame(lat,long) else lines(long,lat,col=rcol,lwd=rlwd,...)}
 if(r0$code!="Ok" | !drive){
 path=gcIntermediate(cbind(x[1],y[1]),cbind(x[2],y[2]),n=101,addStartEnd=T,breakAtDateLine=T)
@@ -20,7 +20,7 @@ rlines=function(x,y,data=F,...){
   loc=paste(paste(x[1],y[1],sep=","),paste(x[2],y[2],sep=","),sep=";")
   url=paste0("http://router.project-osrm.org/route/v1/driving/",loc,"?geometries=geojson&overview=full")
   r0=GET(url); r0=content(r0); if(r0$code=="Ok"){rx=unlist(r0$routes[[1]]$geometry$coordinates); n=length(rx)/2; sn=seq(1,n,by=10)
-  long=rx[1:n*2-1][sn]+runif(1,-1,1)/10; lat=rx[1:n*2][sn]+runif(1,-1,1)/10
+  long=rx[1:n*2-1][sn]; lat=rx[1:n*2][sn]
   if(data) data.frame(lat,long) else lines(long,lat,...)}}
 
 glines=function(x,y,data=F,...){
