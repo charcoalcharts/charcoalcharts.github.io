ac49dc74f312f2f125260b88f14c3bef9c6ba582
charcoalcharts <39935741+charcoalcharts@users.noreply.github.com>
2025-04-01 17:58:04 -0400

    Update ak-hi.R

--- r-scripts/ak-hi.R
+++ r-scripts/ak-hi.R
@@ -1,5 +1,6 @@
 install.if=function(x) if(!x%in%installed.packages()[,"Package"]) install.packages(x)
 install.if("maps")
+library(maps)
 
 alaska=function(...){
 m=map("world","USA:Alaska",plot=F)
@@ -11,3 +12,7 @@ uscan=function(...){
 m=map("world",c("USA","Canada"),plot=F)
 mx=c(); for(j in 1:length(m$names)) mx[j]=max(map("world",m$names[j],plot=F)$x,na.rm=T)
 map("world",c(m$names[mx<0],"USA","USA:Alaska"),exact=T,...)}
+
+#par(fig=c(0,1,0,1)); map("usa",xpd=T)
+add.alaska=function(fig=c(0,0,.3,.3),...){par(new=T,fig=fig[c(1,3,2,4)],mar=rep(0,4)); plot(alaska(plot=F),type="n",bty="n",xaxt="n",yaxt="n"); alaska(add=T)}
+add.hawaii=function(fig=c(.25,.05,.45,.25),...){par(new=T,fig=fig[c(1,3,2,4)],mar=rep(0,4)); plot(hawaii(plot=F),type="n",bty="n",xaxt="n",yaxt="n"); hawaii(add=T)}
