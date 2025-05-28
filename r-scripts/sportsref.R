3ab5c5204afda9a305781c84c285e79d203f15fa
charcoalcharts <39935741+charcoalcharts@users.noreply.github.com>
2025-05-28 13:17:13 -0400

    Update sportsref.R

--- r-scripts/sportsref.R
+++ r-scripts/sportsref.R
@@ -7,3 +7,4 @@ library(rvest)
 
 read_html2=function(url,ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"){Sys.sleep(runif(1,3,7)); GET(url,user_agent(ua)) %>% read_html}
 read_sportsref=function(url){Sys.sleep(runif(1,3,7)); ua=paste(c(sample(LETTERS,10,T),sample(10000,10,T)),collapse=""); GET(url,user_agent(ua)) %>% read_html}
+GET2=function(url){Sys.sleep(runif(1,3,7)); ua=paste(c(sample(LETTERS,10,T),sample(10000,10,T)),collapse=""); GET(url,user_agent(ua))}
