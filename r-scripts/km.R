76a61dfd99d6d940961580f81cc033f00bee4b37
charcoalcharts <39935741+charcoalcharts@users.noreply.github.com>
2026-07-22 12:52:25 -0400

    Update km.R

--- r-scripts/km.R
+++ r-scripts/km.R
@@ -29,3 +29,132 @@ b=a*sqrt(1-e**2) #semiminor axis
 m=function(lat) atan(b*tan(pi*lat/180)/a)
 circ=function(lat) 2*pi*a*cos(m(lat))
 circ(lat)/circ(0)}
+
+fillCircle <- function(LatDec, LonDec, Km, n = 360, ...) {
+  
+  ER <- 6371
+  
+  AngDeg <- seq(0, 359, length.out = n)
+  AngRad <- AngDeg*pi/180
+  
+  Lat1Rad <- LatDec*pi/180
+  Lon1Rad <- LonDec*pi/180
+  d <- Km/ER
+  
+  Lat2Rad <- asin(
+    sin(Lat1Rad)*cos(d) +
+      cos(Lat1Rad)*sin(d)*cos(AngRad)
+  )
+  
+  Lon2Rad <- Lon1Rad +
+    atan2(
+      sin(AngRad)*sin(d)*cos(Lat1Rad),
+      cos(d)-sin(Lat1Rad)*sin(Lat2Rad)
+    )
+  
+  Lat2Deg <- Lat2Rad*180/pi
+  Lon2Deg <- (Lon2Rad*180/pi + 180) %% 360 - 180
+  
+  ####################################################
+  ## Does the boundary wrap around the world?
+  ####################################################
+  
+  dateline <- diff(range(Lon2Deg)) > 350
+  
+  ddeg <- d*180/pi
+  
+  north <- (LatDec + ddeg > 90)
+  south <- (LatDec - ddeg < -90)
+  
+
+  if((north & !south) | (north & dateline)){
+    
+    i1 <- which.min(Lon2Deg+1000*(Lat2Deg<median(Lat2Deg)))
+    i2 <- which.max(Lon2Deg+1000*(Lat2Deg>median(Lat2Deg)))
+    
+    lat1 <- Lat2Deg[i1]
+    lat2 <- Lat2Deg[i2]
+    
+    if(i1 < i2){
+      
+      Lon2Deg <- append(
+        Lon2Deg,
+        c(-180,-180,180,180),
+        after=i1)
+      
+      Lat2Deg <- append(
+        Lat2Deg,
+        c(lat1,90,90,lat2),
+        after=i1)
+      
+    } else {
+      
+      Lon2Deg <- append(
+        Lon2Deg,
+        c(180,180,-180,-180),
+        after=i2)
+      
+      Lat2Deg <- append(
+        Lat2Deg,
+        c(lat2,90,90,lat1),
+        after=i2)
+      
+    }
+  }
+  
+  if((!north & south) | (south & dateline)){
+    
+    i1 <- which.min(Lon2Deg+1000*(Lat2Deg>median(Lat2Deg)))
+    i2 <- which.max(Lon2Deg+1000*(Lat2Deg<median(Lat2Deg)))
+    
+    lat1 <- Lat2Deg[i1]
+    lat2 <- Lat2Deg[i2]
+    
+    if(i1 < i2){
+      
+      Lon2Deg <- append(
+        Lon2Deg,
+        c(-180,-180,180,180),
+        after=i1)
+      
+      Lat2Deg <- append(
+        Lat2Deg,
+        c(lat1,-90,-90,lat2),
+        after=i1)
+      
+    } else {
+      
+      Lon2Deg <- append(
+        Lon2Deg,
+        c(180,180,-180,-180),
+        after=i2)
+      
+      Lat2Deg <- append(
+        Lat2Deg,
+        c(lat2,-90,-90,lat1),
+        after=i2)
+      
+    }
+  }
+  
+  if(north & south & !dateline){
+    
+    i0 <- which.min(Lat2Deg)
+    
+    lat0 <- Lat2Deg[i0]
+    lon0 <- Lon2Deg[i0]
+    
+    Lon2Deg <- append(
+      Lon2Deg,
+      c(lon0,lon0,-180,-180,180,180,lon0,lon0),
+      after=i0)
+    
+    Lat2Deg <- append(
+      Lat2Deg,
+      c(lat0,-90,-90,90,90,-90,-90,lat0),
+      after=i0)
+    
+  }
+  
+  polygon(Lon2Deg, Lat2Deg, fill=T, ...)
+}
